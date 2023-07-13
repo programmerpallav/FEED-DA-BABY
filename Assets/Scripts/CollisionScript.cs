5e365e9c6a5a2aeed90942a6dd582760eca4fe36
Piyush Pallav <lon.18769@gmail.com>
2023-07-13 10:21:07 +0530

    Stars added and baby model

--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour
 {
-    public GameObject scoreText;
     public GameObject levelComplete;
     public GameObject GameOver;
+    public GameObject ScoreUI;
     private void OnTriggerEnter(Collider other)
+
     {
 
         if (other.tag == "Spoon" && gameObject.tag == "Items")
@@ -16,13 +17,14 @@ public class CollisionScript : MonoBehaviour
         if (other.tag == "Spoon" && gameObject.tag == "Child")
         {
             levelComplete.SetActive(true);
+            ScoreUI.SetActive(false);
             Time.timeScale = 0;
         }
 
-        if (other.tag == "Spoon" && gameObject.tag == "Obstacle")
+        //if (other.tag == "Spoon" && gameObject.tag == "Obstacle")
         {
-            GameOver.SetActive(true);
-            Time.timeScale = 0;
+            //GameOver.SetActive(true);
+            //Time.timeScale = 0;
         }
     }
 }
--- Assets/Scripts/PointCollection.cs
+++ Assets/Scripts/PointCollection.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class PointCollection : MonoBehaviour
 {
     private int score;
     public TextMeshProUGUI scoreText;
+    public AudioSource collectiblesSfx;
+    public AudioSource obstaclesSfx;
+    public AudioSource babyGiggle;
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Items"))
         {
+            collectiblesSfx.Play();
             Destroy(collision.gameObject);
             score++;
-            scoreText.text = score.ToString() + "points";
+            scoreText.text = score.ToString() + " " + "points";
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            obstaclesSfx.Play();
+            Destroy(collision.gameObject);
+            score--;
+            scoreText.text = score.ToString() + " " + "points";
+        }
+
+        if (collision.gameObject.CompareTag("Child"))
+        {
+            babyGiggle.Play();
         }
     }
 }
